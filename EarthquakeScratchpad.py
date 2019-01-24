5de7dbaaaffbe21a6966ff68e80d5237debb81c3
Migdalin <migdalin@gmail.com>
2019-01-24 18:55:13 -0500

    Update EarthquakeScratchpad.py

--- EarthquakeScratchpad.py
+++ EarthquakeScratchpad.py
@@ -8,10 +8,18 @@ import csv
 inputFile = open("train.csv", "r", newline='')
 csvReader = csv.reader(inputFile)
 
+# Skip header
+csvReader.__next__()
+prevLabel = 120.0
+
 numRows = 0
 while True:
     try:
-        csvReader.__next__()
+        row = csvReader.__next__()
+        curLabel = float(row[1])
+        if(prevLabel < curLabel):
+            print(f"Discontinuity.  Row: {numRows}; Prev:  {prevLabel:.12f}; Cur:  {curLabel: .23f}")
+        prevLabel = curLabel
         numRows += 1
     except StopIteration:
         break
